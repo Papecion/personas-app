faef05e8720bca03fdb9837c9c10c8227da85aeb
Pablo reyes <pabloandresreyes2004@gmail.com>
2023-10-13 15:24:03 -0500

    Comunne Delete

--- resources/views/comunas/index.blade.php
+++ resources/views/comunas/index.blade.php
@@ -33,6 +33,14 @@
                         <td>{{ $comuna->comu_nomb }}</td>
                         <td>{{ $comuna->muni_nomb }}</td>
                         <td><span>Actions</span></td>
+                        <td>
+                            <form action="{{ route('comunas.destroy', ['comuna' => $comuna->comu_codi]) }}"
+                                method="POST" style="display: inline-block">
+                                @method('delete')
+                                @csrf
+                                <input class="btn btn-danger" type="submit" value="Delete">
+                            </form>
+                        </td>
                     </tr>
                 @endforeach
             </tbody>
