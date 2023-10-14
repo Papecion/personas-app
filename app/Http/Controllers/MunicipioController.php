9575b345800fcc3e5a1b81c84d8d942ecc811856
Pablo reyes <pabloandresreyes2004@gmail.com>
2023-10-14 15:50:17 -0500

    controller municipio y modelo departamento

--- app/Http/Controllers/MunicipioController.php
+++ app/Http/Controllers/MunicipioController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Municipio;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MunicipioController extends Controller
 {
@@ -14,9 +15,11 @@ class MunicipioController extends Controller
      */
     public function index()
     {
-
-        $municipios = Municipio::all();
-        return view('municipios.index', ['municipios' => $municipios]);
+        $municipios = DB::table('tb_municipio')
+            ->join('tb_departamento', 'tb_municipio.depa_codi', '=', 'tb_departamento.depa_codi')
+            ->select('tb_municipio.*', 'tb_departamento.depa_nomb')
+            ->get();
+        return view("municipios.index", ["municipios" => $municipios]);
     }
 
     /**
