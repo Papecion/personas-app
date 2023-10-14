6092411494d43018e4afceb3552ece66a5477055
Pablo reyes <pabloandresreyes2004@gmail.com>
2023-10-14 02:17:11 -0500

    Index municipio y su controller

---
+++ resources/views/municipios/index.blade.php
@@ -0,0 +1,57 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>Munipality List</title>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS v5.2.1 -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+
+</head>
+
+<body>
+    <header>
+    </header>
+    <main>
+        <div class="container">
+            <h1>Listas de municipio</h1>
+            <a href="{{ route('municipios.create') }} " class="btn btn-success">añadir</a>
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th scope="col">code</th>
+                        <th scope="col">Municipio</th>
+                        <th scope="col">Departamento</th>
+                        <th scope="col">Acciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($municipios as $municipio)
+                        <tr>
+                            <th scope="row">{{ $municipio->muni_codi }}</th>
+                            <td>{{ $municipio->muni_nomb }}</td>
+                            <td>{{ $municipio->depa_nomb }}</td>
+                            <td>@mdo</td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+    </main>
+    <footer>
+        <!-- place footer here -->
+    </footer>
+    <!-- Bootstrap JavaScript Libraries -->
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+        integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous">
+    </script>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+        integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
