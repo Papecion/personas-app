b0237e00b656ab9d48dc4acf0353de7a77f289da
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-10-27 16:05:04 -0500

    Instalacion breeze con migraciones

--- routes/web.php
+++ routes/web.php
@@ -1,34 +1,31 @@
 <?php
 
-use App\Http\Controllers\ComunaController;
-use App\Http\Controllers\DepartamentoController;
-use App\Http\Controllers\MunicipioController;
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-//COMUNAS 
-Route::get('/comunas', [ComunaController::class, 'index'])->name('comunas.index');
-Route::post('/comunas', [ComunaController::class, 'store'])->name('comunas.store');
-Route::get('/comunas/create', [ComunaController::class, 'create'])->name('comunas.create');
-Route::delete('/comunas/{comuna}', [ComunaController::class, 'destroy'])->name('comunas.destroy');
-route::put('/comunas/{comuna}', [ComunaController::class, 'update'])->name('comunas.update');
-route::get('/comunas/{comuna}/edit', [ComunaController::class, 'edit'])->name('comunas.edit');
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-//MUNICIPIOS
-route::get('/municipios', [MunicipioController::class, 'index'])->name('municipios.index');
-route::post('/municipios', [MunicipioController::class, 'store'])->name('municipios.store');
-route::get('/municipios/create', [MunicipioController::class, 'create'])->name('municipios.create');
-Route::delete('/municipios/{municipio}', [MunicipioController::class, 'destroy'])->name('municipios.destroy');
-route::put('/municipios/{municipio}', [MunicipioController::class, 'update'])->name('municipios.update');
-route::get('/municipios/{municipio}/edit', [MunicipioController::class, 'edit'])->name('municipios.edit');
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-//DEPARTAMENTOS
-route::get('/departamentos', [DepartamentoController::class, 'index'])->name('departamentos.index');
-route::post('/departamentos', [DepartamentoController::class, 'store'])->name('departamentos.store');
-route::get('/departamentos/create', [DepartamentoController::class, 'create'])->name('departamentos.create');
-Route::delete('/departamentos/{departamento}', [DepartamentoController::class, 'destroy'])->name('departamentos.destroy');
-route::put('/departamentos/{departamento}', [DepartamentoController::class, 'update'])->name('departamentos.update');
-route::get('/departamentos/{departamento}/edit', [DepartamentoController::class, 'edit'])->name('departamentos.edit');
+require __DIR__.'/auth.php';
